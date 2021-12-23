8b4bbc2286d463cf8300447de43a3dcca8de33d1
Vanja <58308535+vanja-veapi@users.noreply.github.com>
2021-12-23 18:00:30 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
-import Login from "./components/Login/Login";
+import NotFound from "./components/NotFound/NotFound";
+import Register from "./components/Register/Register";
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
-			<Login />
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/join" element={<Register />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
 		</div>
 	);
 }
