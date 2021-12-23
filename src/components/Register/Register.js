ef64e8ba8e68b8d9f1152cab5313bc2149caab3c
vanja-veapi <hrastivoje@gmail.com>
2021-12-23 17:49:04 +0100

    Header commit 2

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,22 +5,22 @@ const Register = () => {
 	return (
 		<div className="wrapper d-flex flex-column justify-content-center align-items-center">
 			<div className="container container-form">
-				<h2>u-Team register</h2>
+				<h1>u-Team register</h1>
 
 				<form action="#">
-					<div className="form-group mt-3">
+					<div className="mt-3">
 						<label htmlFor="">Name</label>
 						<input type="text" name="" id="name" placeholder="Name" className="form-control" />
 					</div>
-					<div className="form-group mt-3">
+					<div className="mt-3">
 						<label htmlFor="">Email</label>
 						<input type="text" name="" id="email" placeholder="Email" className="form-control" />
 					</div>
-					<div className="form-group mt-3">
+					<div className="mt-3">
 						<label htmlFor="">Password</label>
 						<input type="text" name="" id="password" placeholder="Password" className="form-control" />
 					</div>
-					<div className="form-group mt-3 mb-3">
+					<div className="mt-3 mb-3">
 						<label htmlFor="">Profile photo</label>
 						<input type="file" name="" id="profile-photo" placeholder="Profile photo" className="profile-photo-btn form-control" />
 					</div>
